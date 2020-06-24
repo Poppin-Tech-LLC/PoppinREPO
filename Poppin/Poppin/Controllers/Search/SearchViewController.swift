8c991bffd86f8576c9a5e8999e5c4ad62af86544
Manuel Martin <manuel.martincallejo@du.edu>
2020-06-24 17:22:11 +0200

    Added search view controller

---
+++ Poppin/Poppin/Controllers/Search/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  Poppin
+//
+//  Created by Manuel Alejandro Martin Callejo on 6/24/20.
+//  Copyright © 2020 Poppin Tech LLC. All rights reserved.
+//
+
+import Foundation
