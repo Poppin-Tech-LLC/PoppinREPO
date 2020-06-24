c368fbd705eb28791e53dec5fd166bad2c3321b1
Manuel Martin <manuel.martincallejo@du.edu>
2020-06-24 18:00:48 +0200

    Added shimmer effect to extensions and updated profile view controller

--- Poppin/Poppin/Controllers/Profile/ProfileViewController.swift
+++ Poppin/Poppin/Controllers/Profile/ProfileViewController.swift
@@ -256,7 +256,7 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate {
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
     {
         print("IMAGE TAPPED")
-        let vc = ViewImageViewController()
+        let vc = ProfileImageViewController()
         vc.uid = uid
         vc.modalPresentationStyle = .overCurrentContext
         self.present(vc, animated: true, completion: nil)
@@ -381,5 +381,101 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate {
         
     }
     
+}
+
+final class ProfileImageViewController: UIViewController {
+
+    var uid: String?
+    var storage: Storage?
+    
+    lazy var imageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: view.bounds.width * 0.8, height: view.bounds.width * 0.8)
+        // userImage.addShadowAndRoundCorners(cornerRadius: userImage.bounds.height/2)
+         imageView.layer.cornerRadius = imageView.bounds.height/2
+        return imageView
+        
+    }()
+    
+    lazy var backButton: ImageBubbleButton = {
+        
+         let purpleArrow = UIImage(systemName: "arrow.left.circle.fill")!.withTintColor(.mainDARKPURPLE)
+         let backButton = ImageBubbleButton(bouncyButtonImage: purpleArrow)
+         backButton.contentMode = .scaleToFill
+        // backButton.setTitle("Back", for: .normal)
+         //backButton.setTitleColor(.newPurple, for: .normal)
+         backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+         return backButton
+        
+     }()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        storage = Storage.storage()
+        
+        let reference = (self.storage?.reference().child("images/\(uid!)/profilepic.jpg"))!
+
+        // Placeholder image
+        let placeholderImage = UIImage.defaultUserPicture256
+
+        // Load the image using SDWebImage
+        //cell.userImageHolder.sd_setImage(with: reference, placeholderImage: placeholderImage)
+        imageView.sd_setImage(with: reference, placeholderImage: placeholderImage)
+        
+        view.backgroundColor = UIColor(white: 0, alpha: 0.9)
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
+
+        view.addSubview(imageView)
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: view.bounds.width * 0.8).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.8).isActive = true
+        
+        view.addSubview(backButton)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.bounds.width * 0.04).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: view.bounds.height * 0.04).isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: view.bounds.height * 0.04).isActive = true
+
+    }
+    
+    @objc func goBack() {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    var viewTranslation = CGPoint(x: 0, y: 0)
+
+    @objc func handleDismiss(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+          case .changed:
+              viewTranslation = sender.translation(in: view)
+              UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                  self.view.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+              })
+          case .ended:
+              if viewTranslation.y < 200 {
+                  UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                      self.view.transform = .identity
+                  })
+              } else {
+                  dismiss(animated: true, completion: nil)
+              }
+          default:
+              break
+          }
+        
+    }
+
 }
 
--- Poppin/Poppin/Miscellaneous/Extensions.swift
+++ Poppin/Poppin/Miscellaneous/Extensions.swift
@@ -176,6 +176,74 @@ extension UIView {
         
     }
     
+    public enum ShimmerDirection: Int {
+        
+        case topToBottom = 0
+        case bottomToTop
+        case leftToRight
+        case rightToLeft
+        case diagonal
+        
+    }
+    
+    public func shimmer(animationSpeed: Float = 2.0, direction: ShimmerDirection = .diagonal, repeatCount: Float = 1) {
+        
+        // Create color  ->2
+        let lightColor = UIColor(displayP3Red: 1.0, green: 1.0, blue: 1.0, alpha: 0.1).cgColor
+        let blackColor = UIColor.black.cgColor
+        
+        // Create a CAGradientLayer  ->3
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [blackColor, lightColor, blackColor]
+        gradientLayer.frame = CGRect(x: -self.bounds.size.width, y: -self.bounds.size.height, width: 3 * self.bounds.size.width, height: 3 * self.bounds.size.height)
+        
+        switch direction {
+        case .topToBottom:
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+            
+        case .bottomToTop:
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+            
+        case .leftToRight:
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+            
+        case .rightToLeft:
+            gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.5)
+            
+        case .diagonal:
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        }
+        
+        gradientLayer.locations =  [0.35, 0.50, 0.65] //[0.4, 0.6]
+        self.layer.mask = gradientLayer
+        
+        // Add animation over gradient Layer  ->4
+        CATransaction.begin()
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [0.0, 0.1, 0.2]
+        animation.toValue = [0.8, 0.9, 1.0]
+        animation.duration = CFTimeInterval(animationSpeed)
+        animation.repeatCount = repeatCount
+        CATransaction.setCompletionBlock { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.layer.mask = nil
+        }
+        gradientLayer.add(animation, forKey: "shimmerAnimation")
+        CATransaction.commit()
+        
+    }
+    
+    public func stopShimmeringAnimation() {
+        
+        self.layer.mask = nil
+        
+    }
+    
 }
 
 extension UITextField {
@@ -213,7 +281,17 @@ extension UITextView {
     
 }
 
-
+extension CGPoint {
+    
+    static func random(_ range: Float) -> CGPoint {
+        
+        let x = Int(-range + (Float(arc4random_uniform(1000)) / 1000.0) * 2.0 * range)
+        let y = Int(-range + (Float(arc4random_uniform(1000)) / 1000.0) * 2.0 * range)
+        return CGPoint(x: x, y: y)
+        
+    }
+    
+}
 
 extension CGFloat {
     
