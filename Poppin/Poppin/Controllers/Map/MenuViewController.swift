70696bdc25da3d2d4db6a158c43fbc7abe736183
Manuel Martin <manuel.martincallejo@du.edu>
2020-06-25 09:36:47 +0200

    Removed unnecessary file carried over from merge

--- Poppin/Poppin/Controllers/Map/MenuViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MenuViewController.swift
-//  Poppin
-//
-//  Created by Manuel Alejandro Martin Callejo on 6/25/20.
-//  Copyright © 2020 Poppin Tech LLC. All rights reserved.
-//
-
-import Foundation
