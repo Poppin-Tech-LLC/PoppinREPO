a4854c88d222178290e51b0e349f825fd8c1ae8f
Manuel Martin <manuel.martincallejo@du.edu>
2020-06-26 18:44:41 +0200

    Finished menu design. Only left is button actions.

--- Poppin/Poppin/Controllers/Menu/MenuViewController.swift
+++ Poppin/Poppin/Controllers/Menu/MenuViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 final class MenuViewController: UIViewController {
     
-    let menuInsetY: CGFloat = .getPercentageWidth(percentage: 5)
+    let menuInsetY: CGFloat = .getPercentageWidth(percentage: 4)
     let menuInsetX: CGFloat = .getPercentageWidth(percentage: 3)
-    let menuInnerInset: CGFloat = .getPercentageWidth(percentage: 3)
+    let menuInnerInset: CGFloat = .getPercentageWidth(percentage: 5.5)
     
     private var fullName: String = "Manuel A Martin Callejo"
     private var username: String = "@manolito_martin"
@@ -23,8 +23,8 @@ final class MenuViewController: UIViewController {
         
         let menuProfileButtonSpacing: CGFloat = .getPercentageWidth(percentage: 4)
         let menuUsernameSpacing: CGFloat = .getPercentageWidth(percentage: 2.5)
-        let menuPopsicleBorderSpacing: CGFloat = .getPercentageWidth(percentage: 2)
         let menuFollowingFollowersSpacing: CGFloat = .getPercentageWidth(percentage: 1)
+        let menuTopStackViewSpacing: CGFloat = .getPercentageWidth(percentage: 3)
         
         let menuFollowingFollowersStackView = UIStackView(arrangedSubviews: [menuFollowingButton, menuFollowersButton])
         menuFollowingFollowersStackView.axis = .horizontal
@@ -32,20 +32,13 @@ final class MenuViewController: UIViewController {
         menuFollowingFollowersStackView.distribution = .fill
         menuFollowingFollowersStackView.spacing = menuFollowingFollowersSpacing
         
-        let menuPopsicleBorderImageView = UIImageView(image: UIImage.popsicleBorder1024.withTintColor(.white))
-        menuPopsicleBorderImageView.contentMode = .scaleAspectFit
-        
-        menuPopsicleBorderImageView.translatesAutoresizingMaskIntoConstraints = false
-        menuPopsicleBorderImageView.heightAnchor.constraint(equalToConstant: menuFullNameLabel.intrinsicContentSize.height + .getPercentageWidth(percentage: 1)).isActive = true
-        
-        var menuTopStackView = UIStackView(arrangedSubviews: [menuProfileButton, menuFullNameLabel, menuUsernameLabel, menuFollowingFollowersStackView, menuPopsicleBorderImageView, menu24hScheduleButtonView])
+        var menuTopStackView = UIStackView(arrangedSubviews: [menuProfileButton, menuFullNameLabel, menuUsernameLabel, menuFollowingFollowersStackView])
         menuTopStackView.axis = .vertical
         menuTopStackView.alignment = .center
         menuTopStackView.distribution = .fill
-        menuTopStackView.spacing = menuInnerInset
+        menuTopStackView.spacing = menuTopStackViewSpacing
         menuTopStackView.setCustomSpacing(menuProfileButtonSpacing, after: menuProfileButton)
         menuTopStackView.setCustomSpacing(menuUsernameSpacing, after: menuFullNameLabel)
-        menuTopStackView.setCustomSpacing(menuPopsicleBorderSpacing, after: menuFollowingFollowersStackView)
         return menuTopStackView
         
     }()
@@ -55,7 +48,7 @@ final class MenuViewController: UIViewController {
         var menuProfileButton = ImageBubbleButton(bouncyButtonImage: UIImage.defaultUserPicture256)
         
         menuProfileButton.translatesAutoresizingMaskIntoConstraints = false
-        menuProfileButton.heightAnchor.constraint(equalToConstant: menuFullNameLabel.intrinsicContentSize.height*4.0).isActive = true
+        menuProfileButton.heightAnchor.constraint(equalToConstant: menuFullNameLabel.intrinsicContentSize.height*3.5).isActive = true
         menuProfileButton.widthAnchor.constraint(equalTo: menuProfileButton.heightAnchor).isActive = true
         
         return menuProfileButton
@@ -139,10 +132,81 @@ final class MenuViewController: UIViewController {
         
     }()
     
+    lazy private var menuButtonsScrollView: UIScrollView = {
+        
+        let menuButtonsStackViewSpacing: CGFloat = .getPercentageWidth(percentage: 9)
+        
+        let menuButtonsStackView = UIStackView(arrangedSubviews: [menu24hScheduleButtonView, menuYourEventsButtonView, menuFriendGroupsButtonView, menuYourSubscriptionButtonView, menuSettingsPrivacyButtonView, menuHelpButtonView])
+        menuButtonsStackView.axis = .vertical
+        menuButtonsStackView.alignment = .leading
+        menuButtonsStackView.distribution = .fill
+        menuButtonsStackView.spacing = menuButtonsStackViewSpacing
+        
+        var menuButtonsScrollView = UIScrollView()
+        menuButtonsScrollView.alwaysBounceVertical = true
+        menuButtonsScrollView.showsVerticalScrollIndicator = false
+        menuButtonsScrollView.automaticallyAdjustsScrollIndicatorInsets = false
+        menuButtonsScrollView.contentInset = UIEdgeInsets(top: .getPercentageWidth(percentage: 9), left: 0.0, bottom: .getPercentageWidth(percentage: 9), right: 0.0)
+        
+        menuButtonsScrollView.addSubview(menuButtonsStackView)
+        menuButtonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        menuButtonsStackView.topAnchor.constraint(equalTo: menuButtonsScrollView.topAnchor).isActive = true
+        menuButtonsStackView.bottomAnchor.constraint(equalTo: menuButtonsScrollView.bottomAnchor).isActive = true
+        menuButtonsStackView.leadingAnchor.constraint(equalTo: menuButtonsScrollView.leadingAnchor).isActive = true
+        menuButtonsStackView.trailingAnchor.constraint(equalTo: menuButtonsScrollView.trailingAnchor).isActive = true
+        menuButtonsStackView.widthAnchor.constraint(equalTo: menuButtonsScrollView.widthAnchor).isActive = true
+        
+        return menuButtonsScrollView
+        
+    }()
+    
+    lazy private var menuPopsicleBorderImageView: UIImageView = {
+        
+        var menuPopsicleBorderImageView = UIImageView(image: UIImage.popsicleBorder1024.withTintColor(.white))
+        menuPopsicleBorderImageView.contentMode = .scaleAspectFit
+        return menuPopsicleBorderImageView
+        
+    }()
+    
     lazy private var menu24hScheduleButtonView: MenuButtonView = {
         
         var menu24hScheduleButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .clockFill), menuButtonText: "24h Schedule")
         return menu24hScheduleButtonView
+        
+    }()
+    
+    lazy private var menuYourEventsButtonView: MenuButtonView = {
+        
+        var menuYourEventsButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .trayFullFill), menuButtonText: "Your Events")
+        return menuYourEventsButtonView
+        
+    }()
+    
+    lazy private var menuFriendGroupsButtonView: MenuButtonView = {
+        
+        var menuFriendGroupsButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .person2Fill), menuButtonText: "Friend Groups")
+        return menuFriendGroupsButtonView
+        
+    }()
+    
+    lazy private var menuYourSubscriptionButtonView: MenuButtonView = {
+        
+        var menuYourSubscriptionButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .starCircleFill), menuButtonText: "Subscription")
+        return menuYourSubscriptionButtonView
+        
+    }()
+    
+    lazy private var menuSettingsPrivacyButtonView: MenuButtonView = {
+        
+        var menuSettingsPrivacyButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .gear), menuButtonText: "Settings")
+        return menuSettingsPrivacyButtonView
+        
+    }()
+    
+    lazy private var menuHelpButtonView: MenuButtonView = {
+        
+        var menuHelpButtonView = MenuButtonView(menuButtonImage: UIImage(systemSymbol: .questionmarkCircleFill), menuButtonText: "Help")
+        return menuHelpButtonView
         
     }()
     
@@ -173,14 +237,27 @@ final class MenuViewController: UIViewController {
         menuTopStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: menuInsetX).isActive = true
         menuTopStackView.trailingAnchor.constraint(equalTo: menuBorderView.leadingAnchor, constant: -menuInsetX).isActive = true
         
+        view.addSubview(menuButtonsScrollView)
+        menuButtonsScrollView.translatesAutoresizingMaskIntoConstraints = false
+        menuButtonsScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: menuInnerInset).isActive = true
+        menuButtonsScrollView.trailingAnchor.constraint(equalTo: menuBorderView.leadingAnchor, constant: -menuInnerInset).isActive = true
+        menuButtonsScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        view.addSubview(menuPopsicleBorderImageView)
+        menuPopsicleBorderImageView.translatesAutoresizingMaskIntoConstraints = false
+        menuPopsicleBorderImageView.widthAnchor.constraint(equalTo: menuButtonsScrollView.widthAnchor).isActive = true
+        menuPopsicleBorderImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        menuPopsicleBorderImageView.topAnchor.constraint(equalTo: menuTopStackView.bottomAnchor).isActive = true
+        menuPopsicleBorderImageView.centerYAnchor.constraint(equalTo: menuButtonsScrollView.topAnchor).isActive = true
+        
     }
     
 }
 
 final class MenuButtonView: UIView {
     
-    let menuButtonInsetY: CGFloat = .getPercentageWidth(percentage: 2)
-    let menuButtonInsetX: CGFloat = .getPercentageWidth(percentage: 1)
+    let menuButtonInsetY: CGFloat = .getPercentageWidth(percentage: 1)
+    let menuButtonInsetX: CGFloat = .getPercentageWidth(percentage: 4)
     
     private var menuButtonText: String?
     private var menuButtonImage: UIImage?
@@ -255,14 +332,15 @@ final class MenuButtonView: UIView {
         
         addSubview(menuButtonLabel)
         menuButtonLabel.centerYAnchor.constraint(equalTo: menuButtonImageView.centerYAnchor).isActive = true
-        menuButtonLabel.leadingAnchor.constraint(equalTo: menuButtonImageView.trailingAnchor, constant: menuButtonInsetX*2).isActive = true
+        menuButtonLabel.leadingAnchor.constraint(equalTo: menuButtonImageView.trailingAnchor, constant: menuButtonInsetX).isActive = true
         menuButtonLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -menuButtonInsetX).isActive = true
         
-        /*addSubview(menuButton)
+        addSubview(menuButton)
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
         menuButton.topAnchor.constraint(equalTo: topAnchor).isActive = true
         menuButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         menuButton.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        menuButton.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true*/
+        menuButton.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         
     }
     
