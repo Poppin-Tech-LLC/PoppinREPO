7b2dcfd383eb40d5668506db17151c9c6c8e863c
Manuel Martin <manuel.martincallejo@du.edu>
2020-07-03 15:17:08 +0200

    Added paging collection view and search page cell.

--- Poppin/Poppin/Controllers/Search/SearchViewController.swift
+++ Poppin/Poppin/Controllers/Search/SearchViewController.swift
@@ -643,24 +643,3 @@ extension SearchViewController: UISearchBarDelegate {
     }
     
 }
-
-final class SelfSizedTableView: UITableView {
-    
-    override var contentSize: CGSize {
-        
-        didSet {
-            
-            invalidateIntrinsicContentSize()
-            
-        }
-        
-    }
-
-    override var intrinsicContentSize: CGSize {
-        
-        layoutIfNeeded()
-        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
-        
-    }
-
-}
