4fccb392df07c4c63b1c3405b4e544979052fa69
Abdulrahman Ayad <abdul.5000@hotmail.co.uk>
2020-07-09 14:02:41 +0100

    Added select university view controller

---
+++ Poppin/Poppin/Controllers/Login/SignUpSelectUniversityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpSelectUniversityViewController.swift
+//  Poppin
+//
+//  Created by Abdulrahman Ayad on 7/9/20.
+//  Copyright © 2020 Poppin Tech LLC. All rights reserved.
+//
+
+import UIKit
+
+class SignUpSelectUniversityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
